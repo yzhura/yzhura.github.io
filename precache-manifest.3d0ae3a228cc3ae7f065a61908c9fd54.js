ed94ff1cece413895a9e577ad082620692a80299
yzhura <zhuravlev.yaroslav@gmail.com>
2020-09-23 11:37:27 +0300

    Updates

---
+++ precache-manifest.3d0ae3a228cc3ae7f065a61908c9fd54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b309d43b2c1070d6ca2317c55e17fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca1888a01ffd99d3cba",
+    "url": "/static/css/main.6934fddb.chunk.css"
+  },
+  {
+    "revision": "c07b529e088394589a14",
+    "url": "/static/js/2.adca306c.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.adca306c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fca1888a01ffd99d3cba",
+    "url": "/static/js/main.aabe8d51.chunk.js"
+  },
+  {
+    "revision": "a2082ef336947dc6f6cf",
+    "url": "/static/js/runtime-main.c0387d80.js"
+  }
+]);
