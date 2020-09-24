5ebe26934e68287a1449b420a0eb8b125b986b41
yzhura <zhuravlev.yaroslav@gmail.com>
2020-09-24 12:27:07 +0300

    gfh

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,6 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
