2d77d546831430ae4cb5ac457574a5b5569f9a3a
yzhura <zhuravlev.yaroslav@gmail.com>
2020-09-24 13:09:12 +0300

    add build folder todo

---
+++ Udemy-JS/ReactJS/todo/build/precache-manifest.98cfb82dcad33b88e880159fbaa1bcdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6452732af52bb9be5f247659fc4c77b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73150a9ff53283ab65bd",
+    "url": "./static/css/main.28bdd390.chunk.css"
+  },
+  {
+    "revision": "a8c771d0198d845294fc",
+    "url": "./static/js/2.01734557.chunk.js"
+  },
+  {
+    "revision": "73150a9ff53283ab65bd",
+    "url": "./static/js/main.05a13d0c.chunk.js"
+  },
+  {
+    "revision": "d2cbf742604520efa1a2",
+    "url": "./static/js/runtime-main.b928f9f7.js"
+  }
+]);
